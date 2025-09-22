503a6fca08f8e002fe737918ed4063e2d60de5b6
Riis12 <202009998@post.au.dk>
2025-09-22 10:47:01 +0200

    Hej, første mappe

---
+++ Manipulator/Procedures/sdf.py
@@ -0,0 +1,18 @@
+import IO
+
+def home_linmot():
+    """
+    Sends a command to home the LinMot motors.
+    """
+    # Create a response and control word with home and switch_on set to True
+    response = IO.Response()
+    control = IO.Control_Word(switch_on=True, home=True)
+    IO.Request(response, control)
+
+    # Create a response and control word with home and switch_on set to False
+    response = IO.Response()
+    control = IO.Control_Word(switch_on=True)
+    IO.Request(response, control)
+
+if __name__ == "__main__":
+    home_linmot()
