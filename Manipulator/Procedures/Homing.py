27c594e8b64c9d528ee4b029711548f5224de64e
Lauritshj <202008735@post.au.dk>
2025-09-22 11:33:52 +0200

    Hej jeg har ændret ting

--- Manipulator/Procedures/Homing.py
+++ Manipulator/Procedures/Homing.py
@@ -1,9 +1,10 @@
 import IO
 
-def home_linmot():
+def home(driver: Driver):
     """
     Sends a command to home the LinMot motors.
     """
+    datagram = IO.linUDP()
     # Create a response and control word with home and switch_on set to True
     response = IO.Response()
     control = IO.Control_Word(switch_on=True, home=True)
@@ -15,4 +16,4 @@ def home_linmot():
     IO.Request(response, control)
 
 if __name__ == "__main__":
-    home_linmot()
+    home()
