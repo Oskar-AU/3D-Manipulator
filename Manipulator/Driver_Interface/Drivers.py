5925d2e9458894dc5bd607f56a8f66a810d1e557
Oskar-AU <201905773@post.au.dk>
2025-09-23 15:09:25 +0200

    Added switch on procedure

--- Manipulator/Driver_Interface/Drivers.py
+++ Manipulator/Driver_Interface/Drivers.py
@@ -14,18 +14,30 @@ class Driver:
         self.name = name
         self.datagram = datagram
 
-    def send(self, request: IO.Request, MC_count: int | None = None, realtime_config_command_count: int | None = None) -> IO.Translated_Response | None:
+    def send(self, request: IO.Request, MC_count: int | None = None, realtime_config_command_count: int | None = None, attempt: int = 1) -> IO.Translated_Response:
         """
         Parameters
         ----------
         request : Request
-            ...
+            The request to send to the drive.
         MC_count : int, optional
             The count of the motion command (4 bits). Must be different than the 
             previous motion command otherwise it is ignored by the drive.
         realtime_config_command_count : int, optional
             The count of the realtime config command (4 bits). Must be different than the 
             previous command otherwise it is ignored by the drive.
+        attempt : int, optional
+            The attempt number for the request. Should not be used.
+
+        Returns
+        -------
+        IO.Translated_Response
+            A dict containing the translated response from the drive. Content depends on the request.
+
+        raises
+        ------
+        TimedOutError
+            If no response is recieved after 5 attempts.
         """
         self.datagram.socket.sendto(request.get_binary(MC_count, realtime_config_command_count), (self.IP, self.port))
         
@@ -48,11 +60,21 @@ class Driver:
 
             return translated_response
         except timeout:
-            logger.warning(f"Response from '{self.name}' timed out (1s).")
+            logger.warning(f"Response from '{self.name}' timed out (1s) at attempt {attempt}/5.")
+            if attempt < 5:
+                return self.send(request, MC_count, realtime_config_command_count, attempt + 1)
+            else:
+                logger.critical(f"Unable to recieve from '{self.name}'.")
+                raise TimeoutError(f"Unable to recieve from '{self.name}'.")
 
     def get_main_state(self) -> int:
         """
         Gets the main state of the drive.
+
+        Returns
+        -------
+        int
+            The main state of the drive.
         """
         return self.send(IO.Request(IO.Response(state_var=True))).get('state_var').get('main_state')
 
@@ -64,6 +86,11 @@ class Driver:
         ----------
         timeout : float, optional
             The time (s) to wait before the homing procedure is considered failed. Default is 30s.
+
+        Returns
+        -------
+        bool
+            Whether or not the procedure ended succesfully.
         """
         logger.info(f"Homing procedure for '{self.name}' initiated.")
 
@@ -91,9 +118,62 @@ class Driver:
         logger.info(f"Homing procedure for '{self.name}' completed.")
         return True
 
+    def switch_on(self, timeout: float = 5) -> bool:
+        """
+        Switches on the drive by setting the main state to 8 from either state 0 or 2.
+
+        Parameters
+        ----------
+        timeout : float, optional
+            The time (s) to wait before considering the switch on procedure failed.
+
+        Returns
+        -------
+        bool
+            Whether or not the procedure ended succesfully.
+        """
+        logger.info(f"Switch on procedure for '{self.name}' initiated.")
+        main_state = self.get_main_state()
+        
+        if main_state == 8:
+            logger.info(f"'{self.name}' already swicthed on. Finishing procedure.")
+            return True
+        if main_state != 2:
+            # Requesting state 2.
+            self.send(IO.Request(IO.Response(), IO.Control_Word()))
+
+            # Waiting for main state to go state 2.
+            if not self.wait_for_change(lambda: self.get_main_state() == 2, timeout=timeout, delay=0.2):
+                logger.error(f"Switch on procedure for '{self.name}' failed: Timed out going to state 2 ({timeout}s). Current state is {self.get_main_state()}.")
+                return False
+
+            main_state = self.get_main_state()
+        
+        if main_state == 2:
+            # Requesting state 8.        
+            self.send(IO.Request(IO.Response(), IO.Control_Word(switch_on=True)))
+
+            # Waiting for state 8.
+            if not self.wait_for_change(lambda: self.get_main_state() == 8, timeout=timeout, delay=0.2):
+                logger.error(f"Switch on procedure for '{self.name}' failed: Timed out going from state 2 to 8 ({timeout}s). Current state is {self.get_main_state()}.")
+                return False
+            
+            # Finalizing.
+            logger.info(f"Switch on procedure for '{self.name}' completed.")
+            return True
+
     def wait_for_change(self, change_checker: Callable[[None], bool], timeout: float, delay: float = 0.0) -> bool:
         """
-        
+        Procedure that waits for a given change to happen in the drive.
+
+        Parameters
+        ----------
+        change_checker : Callable[[None], bool]
+            The criteria to check at 'delay' intervals.
+        timeout : float
+            The amount of time to wait for the change to happen before considering the change to not happen.
+        delay : float, optional
+            The time between checking the criteria. Default is as fast as possible.
         """
         start_time = time.time()
         current_time = time.time()
