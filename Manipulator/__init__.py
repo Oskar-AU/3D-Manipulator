552cc2b3ddce3dd327cc74128bbd6c937333594c
Oskar-AU <201905773@post.au.dk>
2025-10-01 10:11:12 +0200

    Log files are now by default cleared at startup

--- Manipulator/__init__.py
+++ Manipulator/__init__.py
@@ -2,8 +2,9 @@ from .Driver_Interface import IO, Motion_Commands
 from .Manipulator import Manipulator
 from .Stream import Stream
 import logging
+import os
 
-def setup_logging(level: int = logging.DEBUG):
+def setup_logging(level: int = logging.DEBUG, delete_current_log_at_startup: bool = True, file_name: str = "log.log"):
     """
     Optional helper to configure logging. Application can call this but is not automatic.
 
@@ -13,7 +14,7 @@ def setup_logging(level: int = logging.DEBUG):
         The level of logging. Recommended to use e.g. logging.DEBUG (default is logging.DEBUG).
     """
     # Handler for logging in external file.
-    file_handler = logging.FileHandler('log.log')
+    file_handler = logging.FileHandler(file_name)
     file_handler.setLevel(logging.DEBUG)
 
     # Handler for terminal logging.
@@ -25,3 +26,10 @@ def setup_logging(level: int = logging.DEBUG):
         format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
         handlers=[file_handler, terminal_handler]
     )
+
+    if delete_current_log_at_startup and os.path.exists(file_name):
+        try:
+            os.remove(file_name)
+        except OSError as e:
+            logger = logging.getLogger('SETUP')
+            logger.warning(f"Logger was unable to remove previous log file '{file_name}': {e.strerror}. Logs are appended to this file instead.")
